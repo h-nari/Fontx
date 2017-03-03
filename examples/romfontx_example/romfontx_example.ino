95f76bdbb48b6ece19b67a84ea65aac0fa9ac894
Hiroshi Narimatsu <nari@mxb.mesh.ne.jp>
2017-03-03 09:38:49 +0900

    remove unused ota related files

--- examples/romfontx_example/romfontx_example.ino
+++ examples/romfontx_example/romfontx_example.ino
@@ -8,12 +8,15 @@ RomFontx fx(ILGH16XB,ILGZ16XB);
 void setup()
 {
   Serial.begin(115200);
-  delay(100);
+  delay(100); 
   Serial.println("\nReset:");
 
 	const uint8_t *p;
 	uint8_t w,h;
-	uint16_t str[] = {u'漢',u'字',u'T',u'E',u'S',u'T'};
+	uint16_t str[] =
+		{ 0x6f22, // unicode of '漢'
+			0x5b57, // unicode of '字'
+			u'T',u'E',u'S',u'T'};
 
 	for(int i=0;i<sizeof(str)/sizeof(str[0]);i++){
 		if(!fx.getGlyph(str[i], &p, &w, &h)){
